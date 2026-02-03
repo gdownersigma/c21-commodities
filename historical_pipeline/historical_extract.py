324e4dd50004daa05dc0119cdc7e8dfac1f24fc3
11wduong <11wduong@gmail.com>
2026-02-03 16:09:53 +0000

    Finised function

--- historical_pipeline/historical_extract.py
+++ historical_pipeline/historical_extract.py
@@ -5,5 +5,24 @@ from os import environ as ENV
 import pandas as pd
 from dotenv import load_dotenv
 
-API_KEY =
-API_URL = f"https://financialmodelingprep.com/stable/historical-price-eod/full?symbol=GCUSD&apikey={API_KEY}"
+
+
+
+
+def fetch_historical_data(symbol: str) -> pd.DataFrame:
+    """Fetches historical financial data for specified symbol from the given API URL from the last 30 days from today."""
+    API_KEY = ENV.get("API_KEY")
+    API_URL = f"https://financialmodelingprep.com/stable/historical-price-eod/full?symbol={symbol}&apikey={API_KEY}"
+    response = requests.get(API_URL)
+    data = response.json()
+    df = pd.DataFrame(data)
+    df['date'] = pd.to_datetime(df['date'])
+    last_30_days = pd.Timestamp.now() - pd.Timedelta(days=30)
+    df = df[df['date'] >= last_30_days]
+    return df
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    historical_data_df = fetch_historical_data("DCUSD")
+    print(historical_data_df)
