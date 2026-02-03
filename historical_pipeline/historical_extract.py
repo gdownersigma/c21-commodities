531e566c520b7d7aa1c771fc1015b3aec1b62424
11wduong <11wduong@gmail.com>
2026-02-03 16:34:03 +0000

    Further edits

--- historical_pipeline/historical_extract.py
+++ historical_pipeline/historical_extract.py
@@ -9,11 +9,11 @@ from dotenv import load_dotenv
 def fetch_historical_data(symbol: str) -> pd.DataFrame:
     """Fetches historical financial data for specified symbol from the given API URL from the last 30 days from today."""
     API_KEY = ENV.get("API_KEY")
-    API_URL = f"https://financialmodelingprep.com/stable/historical-price-eod/full?symbol={symbol}&apikey={API_KEY}"
+    from_date = (pd.Timestamp.today() -
+                 pd.Timedelta(days=30)).strftime('%Y-%m-%d')
+    to_date = pd.Timestamp.today().strftime('%Y-%m-%d')
+    API_URL = f"https://financialmodelingprep.com/stable/historical-price-eod/full?symbol={symbol}&apikey={API_KEY}&from={from_date}&to={to_date}"
     response = requests.get(API_URL)
     data = response.json()
     df = pd.DataFrame(data)
-    df['date'] = pd.to_datetime(df['date'])
-    last_30_days = pd.Timestamp.now() - pd.Timedelta(days=30)
-    df = df[df['date'] >= last_30_days]
     return df
